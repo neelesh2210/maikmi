8fd62c190454d0e8f385a240a7499f68ce295ad7
neelesh2210 <neelesh2210@gmail.com>
2025-01-13 11:08:41 +0530

    Refactor wallet transactions retrieval in WalletTransactionController

--- app/Http/Controllers/Api/Vendors/WalletTransactionController.php
+++ app/Http/Controllers/Api/Vendors/WalletTransactionController.php
@@ -11,17 +11,17 @@ class WalletTransactionController extends Controller
 {
 
     public function index() {
-        $wallet_transactions = SalonWallet:: where('salon_id', Auth::user()->getSalon->id)->latest()->paginate(50,['amount', 'source', 'type', 'created_at']);
+        return $wallet_transactions = SalonWallet:: where('salon_id', Auth::user()->getSalon->id)->latest()->paginate(50,['amount', 'source', 'type', 'created_at']);
 
         return response()->json([
             'wallet_transactions'=>$wallet_transactions,
-            'links'                     =>[
-                'first_page_url'        => $wallet_transactions->url($wallet_transactions->firstItem()),
-                'last_page_url'         => $wallet_transactions->url($wallet_transactions->lastPage()),
-                'next_page_url'         => $wallet_transactions->nextPageUrl(),
-                'prev_page_url'         => $wallet_transactions->previousPageUrl(),
-            ],
-            'message'=>'Plan Purchase List Retrived Successfully!',
+            // 'links'                     =>[
+            //     'first_page_url'        => $wallet_transactions->url($wallet_transactions->firstItem()),
+            //     'last_page_url'         => $wallet_transactions->url($wallet_transactions->lastPage()),
+            //     'next_page_url'         => $wallet_transactions->nextPageUrl(),
+            //     'prev_page_url'         => $wallet_transactions->previousPageUrl(),
+            // ],
+            'message'=>'Wallet Transaction Retrived Successfully!',
             'status'=>200
         ],200);
     }
